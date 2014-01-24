055973d5b1e0b41d485f53dd22a433d99fdeaa29
Galman33 <galman33@gmail.com>
2014-01-24 16:13:35 +0200

    Added InfinityWorldScrolling Script

---
+++ Assets/Scripts/InfiniteWorldScrolling.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GamePerspective))]
+public class InfiniteWorldScrolling : MonoBehaviour {
+
+    public const float MAX_DISTANCE = 20;
+
+    public GamePerspective scrollingCenterPersp;
+    private GamePerspective persp;
+
+	// Use this for initialization
+	void Start () {
+        persp = GetComponent<GamePerspective>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Mathf.Abs(scrollingCenterPersp.X - persp.X) > MAX_DISTANCE)
+        {
+            if (scrollingCenterPersp.X > persp.X)
+                persp.SetPosition(persp.X + MAX_DISTANCE * 2, persp.Z);
+            else
+                persp.SetPosition(persp.X - MAX_DISTANCE * 2, persp.Z);
+        }
+	}
+}
