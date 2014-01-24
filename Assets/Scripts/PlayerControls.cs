a26491de7e4e69b18a643283952523d755fe9be5
Maayan Glikser <maayan@glikm.com>
2014-01-24 12:11:02 +0200

    Added basic move script

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+    EntityMove move;
+
+	// Use this for initialization
+	void Start () {
+
+        move = GetComponent<EntityMove>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
