1a5d46e99c3856fbad5bdc4d702f378db0bb8514
Galman33 <galman33@gmail.com>
2014-01-24 16:36:43 +0200

    Added Spawner script Added RandomPostionSpawn script Added Coin graphics

---
+++ Assets/Scripts/RandomSpawnPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GamePerspective))]
+public class RandomSpawnPosition : MonoBehaviour {
+
+    public GamePerspective player;
+
+	// Use this for initialization
+	void Start () {
+	    GamePerspective persp = GetComponent<GamePerspective>();
+        persp.SetPosition(player.X + Random.Range(-InfiniteWorldScrolling.MAX_DISTANCE, InfiniteWorldScrolling.MAX_DISTANCE),
+                            Random.Range(WorldBounding.MIN_Z, WorldBounding.MAX_Z));
+	}
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject spawnObject;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+
+    private float nextSpawnTime;
+
+	// Use this for initialization
+	void Start () {
+        SetNextSpawnTime();
+	}
+	
+    private void SetNextSpawnTime()
+    {
+        nextSpawnTime = Time.time + Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if(Time.time >= nextSpawnTime)
+        {
+            Spawn();
+            SetNextSpawnTime();
+        }
+	}
+
+    private void Spawn()
+    {
+        GameObject.Instantiate(spawnObject);
+    }
+
+}
