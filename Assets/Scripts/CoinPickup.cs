0f6b045d7deb0966803b7d5803984f0866e67d57
Maayan Glikser <maayan@glikm.com>
2014-01-25 00:08:18 +0200

    Backgrounds WIP

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+    GameObject[] coins;
+
+   void Start() {
+       coins = GameObject.FindGameObjectsWithTag("Coin");
+
+   }
+
+   void FixedUpdate()
+   {
+       foreach (var coin in coins)
+       {
+           var distance = Vector3.Distance(transform.position, coin.transform.position);
+           Debug.Log(distance);
+       }
+   }
+}
---
+++ Assets/Scripts/ParallaxMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxMove : MonoBehaviour {
+
+    public float speed = 2f;
+
+    float offset = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        offset -= speed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+
+        /*
+        if (offset <= 0.5f)
+        {
+            offset = Mathf.Min(0.5f, offset + (speed * Time.deltaTime));
+            print(offset);
+            
+            renderer.material.mainTextureOffset = new Vector2(offset, 0);
+        }*/
+	
+	}
+}
