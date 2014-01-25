e091f2859b385bd455234c41ea3a881ad22c5f8a
Maayan Glikser <maayan@glikm.com>
2014-01-25 12:03:50 +0200

    Added easin to movement

--- Assets/Scripts/SquidMove.cs
+++ Assets/Scripts/SquidMove.cs
@@ -3,29 +3,25 @@ using System.Collections;
 
 public class SquidMove : MonoBehaviour {
 
-    float lastActionTime;
     float interval = 3f;
     bool canMove = false;
+    float startMoveTime;
 
-    void Start()
-    {
-        lastActionTime = Time.time;
-    }
-
-    void Stop()
+    void StopSquid()
     {
         canMove = false;
     }
 
-    void Move()
+    void MoveSquid()
     {
         //var position = Vector3.Lerp(transform.position, transform.position + new Vector3(1, 0, 0), 0.1f);
         canMove = true;
+        startMoveTime = Time.time;
     }
 
     void Update()
     {
         if (canMove)
-            transform.position = transform.position + Vector3.right * Time.deltaTime;
+            transform.position = transform.position + Vector3.right * Time.deltaTime * (Time.time - startMoveTime) * (Time.time - startMoveTime) * 3;
     }
 }
