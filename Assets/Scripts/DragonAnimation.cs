6861586d85c26f42a4c48411c175e3630534922b
Maayan Glikser <maayan@glikm.com>
2014-01-25 14:39:45 +0200

    Added dragon animation

---
+++ Assets/Scripts/DragonAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonAnimation : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void AfterAppear()
+    {
+        anim.Play("DragonLoop");
+    }
+}
