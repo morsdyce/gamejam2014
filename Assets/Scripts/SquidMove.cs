35d3b5fefa2952250429a3d2b44dda4a11d645cc
Maayan Glikser <maayan@glikm.com>
2014-01-25 11:48:49 +0200

    Day of the space tentacles

---
+++ Assets/Scripts/SquidMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquidMove : MonoBehaviour {
+
+    float lastActionTime;
+    float interval = 3f;
+    bool canMove = false;
+
+    void Start()
+    {
+        lastActionTime = Time.time;
+    }
+
+    void Stop()
+    {
+        canMove = false;
+    }
+
+    void Move()
+    {
+        //var position = Vector3.Lerp(transform.position, transform.position + new Vector3(1, 0, 0), 0.1f);
+        canMove = true;
+    }
+
+    void Update()
+    {
+        if (canMove)
+            transform.position = transform.position + Vector3.right * Time.deltaTime;
+    }
+}
