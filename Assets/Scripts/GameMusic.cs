123b8205f6276814cc9ab2b581fb922e2cebe499
Maayan Glikser <maayan@glikm.com>
2014-01-25 17:30:11 +0200

    Added game music and switching track

---
+++ Assets/Scripts/GameMusic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMusic : MonoBehaviour {
+
+    public AudioClip calm;
+    public AudioClip crazy;
+
+    private Stats stats;
+    int lastCreativity = 0;
+
+    bool isPlayingCalm = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+        stats = FindObjectOfType<Stats>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (stats.creativity > 1 && isPlayingCalm && lastCreativity != stats.creativity)
+        {
+            print("switch to crazy");
+            audio.Stop();
+            audio.clip = crazy;
+            audio.Play();
+            isPlayingCalm = false;
+
+        }
+        else if (!isPlayingCalm && lastCreativity != stats.creativity)
+        {
+            print("switch to calm");
+            audio.Stop();
+            audio.clip = calm;
+            audio.Play();
+            isPlayingCalm = true;
+        }
+
+        lastCreativity = stats.creativity;
+	
+	}
+}
